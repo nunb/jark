232c65fefff69d4859ab2ebe2277455f9db2b870
Isaac Praveen <icylisper@gmail.com>
2011-04-03 21:12:58 +0530

    Added --package & --version options to all package commands

--- modules/package.sh
+++ modules/package.sh
@@ -4,7 +4,7 @@ DOC="Module for working with cljr build system"
 . ${CLJR_BIN}/shflags
 
 commands() {
-    echo -e "install uninstall versions describe deps search installed latest"
+    echo -e "install uninstall versions deps search installed latest"
 }
 
 install() {
@@ -13,7 +13,7 @@ install() {
     FLAGS "$@" || exit 1
     eval set -- "${FLAGS_ARGV}"
     if [ ${FLAGS_package} == "0" ]; then
-        echo "USAGE: jark package install --package [--version]"
+        echo "USAGE: jark package install --package PACKAGE [--version]"
         exit 1
     fi
         
@@ -26,38 +26,70 @@ install() {
 }
 
 uninstall() {
-    $JARK_CLIENT cljr.App uninstall $*
+    DEFINE_string 'package' '0' 'package' 'p'
+    FLAGS "$@" || exit 1
+    eval set -- "${FLAGS_ARGV}"
+    if [ ${FLAGS_package} == "0" ]; then
+        echo "USAGE: jark package uninstall --package PACKAGE"
+        exit 1
+    fi
+    $JARK_CLIENT cljr.App uninstall ${FLAGS_package}
 }
 
 versions() {
-    $JARK_CLIENT cljr.App versions $*
-}
-
-describe() {
-    $JARK_CLIENT cljr.App search $*
+    DEFINE_string 'package' '0' 'package' 'p'
+    FLAGS "$@" || exit 1
+    eval set -- "${FLAGS_ARGV}"
+    if [ ${FLAGS_package} == "0" ]; then
+        echo "USAGE: jark package versions --package PACKAGE"
+        exit 1
+    fi
+    $JARK_CLIENT cljr.App versions ${FLAGS_package}
+    exit 0
 }
 
 search() {
-    $JARK_CLIENT cljr.App search $*
-}
-
-run() {
-    $JARK_CLIENT cljr.App $*
+    DEFINE_string 'package' '0' 'package' 'p'
+    FLAGS "$@" || exit 1
+    eval set -- "${FLAGS_ARGV}"
+    if [ ${FLAGS_package} == "0" ]; then
+        echo "USAGE: jark package search --package PACKAGE"
+        exit 1
+    fi
+    $JARK_CLIENT cljr.App search ${FLAGS_package}
 }
 
 installed() {
     $JARK_CLIENT cljr.App list
 }
 
-deps() {
-    if [ -z $2 ]; then
-        ver=`$JARK cljr.clojars get-latest-version $1`
-        $JARK cljr.clojars print-library-dependencies $1 $ver
-    else
-        $JARK cljr.clojars print-library-dependencies $1 $2
+latest() {
+    DEFINE_string 'package' '0' 'package' 'p'
+    FLAGS "$@" || exit 1
+    eval set -- "${FLAGS_ARGV}"
+    if [ ${FLAGS_package} == "0" ]; then
+        echo "USAGE: jark package latest --package PACKAGE"
+        exit 1
     fi
+    $JARK cljr.clojars get-latest-version ${FLAGS_package}
 }
 
-latest() {
-    $JARK cljr.clojars get-latest-version $*
+deps() {
+    DEFINE_string 'version' '0' 'version' 'v'
+    DEFINE_string 'package' '0' 'package' 'p'
+    FLAGS "$@" || exit 1
+    eval set -- "${FLAGS_ARGV}"
+    if [ ${FLAGS_package} == "0" ]; then
+        echo "USAGE: jark package deps --package PACKAGE [--version]"
+        exit 1
+    fi
+
+    if [ ${FLAGS_version} == "0" ]; then
+        ver=`$JARK cljr.clojars get-latest-version ${FLAGS_package}`
+        $JARK cljr.clojars print-library-dependencies ${FLAGS_package} $ver
+    else
+        $JARK cljr.clojars print-library-dependencies ${FLAGS_package} ${FLAGS_version}
+    fi
+    exit 0
 }
+
