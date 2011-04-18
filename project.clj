32854bd697fbaa38020b69dd8c45a35981ce0865
Isaac Praveen <icylisper@gmail.com>
2011-04-18 16:42:14 +0530

    added repository in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject jark "0.4"
   :description "JARK is a tool to manage classpaths and clojure namespaces on a persistent JVM"
-  :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]]
+  ;; :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]]
 
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
@@ -8,11 +8,11 @@
                  [cljr "1.0.0-SNAPSHOT"]
                  [midje "1.1"]
                  [org.clojure/tools.nrepl "0.0.4"]
-                 [org.cloudhoist/pallet "0.4.15"]
+                 [org.cloudhoist/pallet "0.4.14"]
                  [org.cloudhoist/pallet-crates-all "0.4.2"]]
 
-  :repositories  {"stuartsierra"       "http://stuartsierra.com/maven2"
-                  "sonatype"           "http://oss.sonatype.org/content/repositories/releases/"}
+  :repositories  {"sonatype"
+                  "http://oss.sonatype.org/content/repositories/releases"}
 
   :aot [jark.vm jark.cp jark.ns
         jark.swank jark.doc leiningen.build-jark]) 
