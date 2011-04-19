902e79de6baaf804662352f978d0e049ae24fa69
Isaac Praveen <icylisper@gmail.com>
2011-04-19 19:35:50 +0530

    merge package command fixes from 0.3

--- src/server/project.clj
+++ src/server/project.clj
@@ -7,4 +7,4 @@
                  [org.clojure/tools.nrepl "0.0.4"]
                  [cljr "1.0.0-SNAPSHOT" :exclusions [org.clojure/clojure org.clojure/clojure-contrib]]]
   
-  :aot [jark.vm jark.cp jark.ns jark.swank jark.doc]) 
+  :aot [jark.vm jark.cp jark.ns jark.swank jark.doc jark.package]) 
