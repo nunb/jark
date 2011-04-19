694192a81897f2cb80dab0aa0ef2b59d9881a283
Isaac Praveen <icylisper@gmail.com>
2011-04-19 15:51:38 +0530

    add getopts to jark client (hs)

--- src/client/jarkc.hs
+++ src/client/jarkc.hs
@@ -1,7 +1,38 @@
-import System.Environment
- 
--- | 'main' runs the main program
-main :: IO ()
-main = getArgs >>= print . message . head
- 
-message s = "The beginnings of a superfast jark client " ++ s
+module Main( main ) where
+
+import System
+import System.Console.GetOpt
+import Data.Maybe( fromMaybe )
+
+main = do
+  args <- getArgs
+  let ( actions, nonOpts, msgs ) = getOpt RequireOrder options args
+  opts <- foldl (>>=) (return defaultOptions) actions
+  let Options { optInput = input,
+                optOutput = output } = opts
+  input >>= output
+
+data Options = Options  {
+    optInput  :: IO String,
+    optOutput :: String -> IO ()
+  }
+
+defaultOptions :: Options
+defaultOptions = Options {
+    optInput  = getContents,
+    optOutput = putStr
+  }
+
+options :: [OptDescr (Options -> IO Options)]
+options = [
+    Option ['V'] ["version"] (NoArg showVersion)         "show version number",
+    Option ['i'] ["input"]   (ReqArg readInput "FILE")   "input file to read",
+    Option ['o'] ["output"]  (ReqArg writeOutput "FILE") "output file to write"
+  ]
+
+showVersion _ = do
+  putStrLn "jark client 0.4"
+  exitWith ExitSuccess
+
+readInput arg opt = return opt { optInput = readFile arg }
+writeOutput arg opt = return opt { optOutput = writeFile arg }
