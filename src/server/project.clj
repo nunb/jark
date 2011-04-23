c59d996e994d15bdf6031176c2f664a92cfc051d
Isaac Praveen <icylisper@gmail.com>
2011-04-23 20:04:08 +0530

    merge from 0.3: have saner dependencies

--- src/server/project.clj
+++ src/server/project.clj
@@ -1,10 +1,10 @@
-(defproject jark-server "0.4"
-  :description "JARK server utilities"
-
+(defproject jark "0.3"
+  :description "JARK is a tool to manage classpaths and clojure namespaces on a persistent JVM"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [swank-clojure "1.2.0"]
-                 [org.clojure/tools.nrepl "0.0.4"]
-                 [cljr "1.0.0-SNAPSHOT" :exclusions [org.clojure/clojure org.clojure/clojure-contrib]]]
-  
-  :aot [jark.vm jark.cp jark.ns jark.swank jark.doc jark.package]) 
+                 [swank-clojure "1.3.0"]
+                 [leiningen "1.1.0"]
+                 [org.clojure/tools.nrepl "0.0.4"]]
+
+  :aot [jark.vm jark.cp jark.ns jark.swank jark.doc jark.package
+        cljr.core cljr.clojars]) 
