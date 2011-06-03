7a1fbc20781ce3e1d5815df299f918f76cd4ecf3
Isaac Praveen <icylisper@gmail.com>
2011-06-03 07:33:44 +0530

    fix cli commands; upgrade tools.nrepl dependency to version 0.0.5

--- server/project.clj
+++ server/project.clj
@@ -4,7 +4,7 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [swank-clojure "1.3.0"]
                  [leiningen "1.1.0"]
-                 [org.clojure/tools.nrepl "0.0.4"]]
+                 [org.clojure/tools.nrepl "0.0.5"]]
 
   :aot [jark.vm jark.cp jark.ns jark.swank jark.pp jark.doc jark.package
         cljr.core cljr.clojars]) 
